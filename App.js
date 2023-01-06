f77902d05db22f877dafddd3982b5ef2ec2dcb6e
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-01-06 14:18:28 -0500

    initial setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Loading from './Loading';
+import Tours from './Tours';
+const url = 'https://course-api.com/react-tours-project';
+
+
+function App() {
+  return <h2> Tours project </h2>
+}
+
+export default App;
---
+++ Tours.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = () => {
+    return <h2>Tours Component</h2>;
+};
+
+export default Tours;
